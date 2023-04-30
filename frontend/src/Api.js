238bfbe7d69f8c98db4662b02f57656442e238bf
basicReed <jtreed.itsallgood@gmail.com>
2023-04-30 16:34:14 -0600

    search scroll & user card sizing

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_API_URL = "http://localhost:3001";
+const BASE_API_URL = "https://flashfeedapp.herokuapp.com";
 
 class FlashFeedApi {
   // POST to authenticate user and return JWT token
